96b21f768819c2d1a581e6acbfb4585e8a93e4db
Luis Rioja <luis.rioja11@gmail.com>
2020-10-19 18:04:58 +0200

    Implementado Layout

---
+++ resources/views/pagina01.blade.php
@@ -0,0 +1,15 @@
+@extends('layout.ejemploLayout')
+
+@section('titulo', 'Pagina 01 del proyecto')
+
+@section('barralateral')
+
+<p>Esto es la barra lateral</p>
+
+@endsection
+
+@section('contenido')
+
+<p>Esto esta en el body</p>
+
+@endsection
